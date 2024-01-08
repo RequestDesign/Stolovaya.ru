c4f99284a6554bf67bc14febbe344337ff260006
Artem Bondarchuk <arhipilagys@gmail.com>
2024-01-08 23:54:39 +1100

    artem

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,2 +1,8 @@
-const header = "header";
-console.log(header);
+const searchSelect = document.querySelector('.search-select');
+
+searchSelect.addEventListener('input', resizeInput);
+resizeInput.call(searchSelect)
+
+function resizeInput() {
+    this.style.width = this.value.length + 'em'
+}
