516379fc51ac29c39d506b0ead510f2fdc3d98b0
Artem Bondarchuk <arhipilagys@gmail.com>
2024-01-09 19:36:34 +1100

    made 2 sections

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,3 +1,27 @@
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules';
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+function remToPx(remValue) {
+    var htmlFontSize = parseFloat(
+        getComputedStyle(document.documentElement).fontSize
+    );
+    var pxValue = remValue * htmlFontSize;
+    return Math.round(pxValue) + "px";
+}
+
+const headerSwiper = new Swiper('.swiper', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: `${remToPx(1.6)}`,
+    navigation: {
+        nextEl: '.swiper-button-next'
+    },
+    slidesPerView: 'auto'
+})
+
 const searchSelect = document.querySelector('.search-select');
 
 searchSelect.addEventListener('input', resizeInput);
