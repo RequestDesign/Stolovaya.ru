561b3ff1cf655b2c672293f75dd6df7b0cd36a92
Aleftina17 <teekupinmuotti@gmail.com>
2024-01-16 16:04:09 +0300

    fix: section contact; feat: tablet start

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import "./js/components/header";
 import "./js/components/range";
 import "./js/components/footer";
 import "./js/components/modal";
-import "./js/components/portfolio-swiper";
+import "./js/components/portfolio-section-swiper";
