b29c9e87a5da153ad7e44be659d27cd934c0a4a9
Aleftina17 <teekupinmuotti@gmail.com>
2024-01-16 14:00:44 +0300

    feat: portfolio detail mobile, js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./index.scss";
 import "./js/pages/home";
 import "./js/pages/catalogue";
 import "./js/pages/about";
-import "./js/pages/portfolio"
+// import "./js/pages/portfolio"
+import "./js/pages/project-detail";
 import "./js/components/header";
 import "./js/components/range";
 import "./js/components/footer";
