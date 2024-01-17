f186b97df995b72353d2f0b222c92a75e6eedf66
Aleftina17 <teekupinmuotti@gmail.com>
2024-01-17 17:26:17 +0300

    fix: sections, about js

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,50 +1,65 @@
 import $ from "jquery";
 
 //partners cards
-if ($('.about__clients').length) {
-    $("#loadMorePartnersLink").on("click", function() {
-        $(".partners-card").slideDown();
-        $(this).hide();
+if ($('.about__partners').length) {
+    const $partnersCards = $(".partners-card");
+    const $loadMorePartnersLink = $("#loadMorePartnersLink");
+
+    $loadMorePartnersLink.on("click", function() {
+        const hiddenCards = $partnersCards.filter(':hidden');
+        hiddenCards.slice(0, 4).slideDown();
+        if (hiddenCards.length <= 4) {
+            $loadMorePartnersLink.hide();
+        }
     });
 
     function adjustPartnersVisibility() {
         if ($(window).width() < 769) {
-            $(".partners-card").hide();
-            $(".partners-card:lt(10)").show();
-            $("#loadMorePartnersLink").show();
+            $partnersCards.hide();
+            $partnersCards.slice(0, 10).show();
+            $loadMorePartnersLink.show();
         } else {
-            $(".partners-card").show();
-            $("#loadMorePartnersLink").hide();
+            $partnersCards.show();
+            $loadMorePartnersLink.hide();
         }
     }
+
     adjustPartnersVisibility();
 
     $(window).on('resize', function() {
         adjustPartnersVisibility();
     });
-};
+}
+
 
 //clients cards
 if ($('.about__clients').length) {
-    $("#loadMoreClientsLink").on("click", function() {
-        $(".card-clients").slideDown();
-        $(this).hide();
+    const $clientsCards = $(".card-clients");
+    const $loadMoreClientsLink = $("#loadMoreClientsLink");
+
+    $loadMoreClientsLink.on("click", function() {
+        const hiddenCards = $clientsCards.filter(':hidden');
+        hiddenCards.slice(0, 4).slideDown();
+        if (hiddenCards.length <= 4) {
+            $loadMoreClientsLink.hide();
+        }
     });
 
     function adjustClientsVisibility() {
         if ($(window).width() < 769) {
-            $(".card-clients").hide();
-            $(".card-clients:lt(10)").show();
-            $("#loadMoreClientsLink").show();
+            $clientsCards.hide();
+            $clientsCards.slice(0, 10).show();
+            $loadMoreClientsLink.show();
         } else {
-            $(".card-clients").show();
-            $("#loadMoreClientsLink").hide();
+            $clientsCards.show();
+            $loadMoreClientsLink.hide();
         }
     }
+
     adjustClientsVisibility();
 
     $(window).on('resize', function() {
         adjustClientsVisibility();
     });
-};
+}
 
