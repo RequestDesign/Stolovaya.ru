81c293ba6c18b3ad423731166f79d697ac99478d
Aleftina17 <teekupinmuotti@gmail.com>
2024-01-22 13:27:04 +0300

    fix: range inputs

--- src/js/pages/request.js
+++ src/js/pages/request.js
@@ -18,11 +18,3 @@ $(function () {
     });
 });
 
-// document.querySelectorAll(".request__input textarea").forEach((el) => {
-//     el.style.height = el.setAttribute("style", "height: " + el.scrollHeight + "px");
-//     el.classList.add("auto");
-//     el.addEventListener("input", (e) => {
-//         el.style.height = "auto";
-//         el.style.height = el.scrollHeight + "px";
-//     });
-// });
