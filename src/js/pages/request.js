f274525f378415f4962ae98f0a99490dfdbe22a0
Aleftina17 <teekupinmuotti@gmail.com>
2024-01-23 12:00:52 +0300

    feat: phone mask

--- src/js/pages/request.js
+++ src/js/pages/request.js
@@ -1,13 +1,6 @@
-import IMask from "imask";
 import $ from "jquery";
 
 $(function () {
-    if ($("#phone-mask").length) {
-        IMask(document.getElementById("phone-mask"), {
-            mask: "+{7}(000)000-00-00",
-        });
-    }
-
     $(".request__input input, .request__input textarea").on("input", function () {
         let $parentLabel = $(this).closest(".request__input");
         if ($(this).val().trim() !== "") {
